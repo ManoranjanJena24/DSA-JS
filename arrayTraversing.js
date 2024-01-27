ffc0c159de5ea688e51b9d5a7fe927112dae9f36
ManoranjanJena24 <mjena@mitaoe.ac.in>
2024-01-27 10:44:47 +0530

    array Traversal

--- arrayTraversing.js
+++ arrayTraversing.js
@@ -1,4 +1,27 @@
 let data = [23, 46, 54, 12, 25, 44, 33, 26]
-for (let i = 0; i < data.length; i++){
-    document.write(`Array ${i}  is ${data[i]} <br>`)
-}
+// for (let i = 0; i < data.length; i++){
+//     document.write(`Array ${i}  is ${data[i]} <br>`)
+// }
+
+// let x = 3;
+// document.write(data[x]);
+function getElement() {
+    let index = document.getElementById("element").value;
+    if (index < data.length) {
+        alert(data[index])
+    }
+    else {
+        alert("out of scope man")
+    }
+} 
+
+
+function getElement() {
+    let index = document.getElementById("element").value;
+    if (index < data.length) {
+        alert(data[index])
+    }
+    else {
+        alert("out of scope man")
+    }
+} 
