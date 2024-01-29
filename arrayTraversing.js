9c464a88fb35fcce981bee89b2508b8da745d4df
ManoranjanJena24 <mjena@mitaoe.ac.in>
2024-01-29 09:18:07 +0530

    array traversing validation some additional features

--- arrayTraversing.js
+++ arrayTraversing.js
@@ -5,20 +5,22 @@ let data = [23, 46, 54, 12, 25, 44, 33, 26]
 
 // let x = 3;
 // document.write(data[x]);
-function getElement() {
-    let index = document.getElementById("element").value;
-    if (index < data.length) {
-        alert(data[index])
-    }
-    else {
-        alert("out of scope man")
-    }
-} 
+
+
+// function getElement() {
+//     let index = document.getElementById("element").value;
+//     if (index < data.length) {
+//         alert(data[index])
+//     }
+//     else {
+//         alert("out of scope man")
+//     }
+// } 
 
 
 function getElement() {
     let index = document.getElementById("element").value;
-    if (index < data.length) {
+    if (index < data.length && typeof parseInt(index)==="number" ) {
         alert(data[index])
     }
     else {
