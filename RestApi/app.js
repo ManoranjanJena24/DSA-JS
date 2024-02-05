6e9840fcacec2feeda3d61c1ab275c0b359f5d43
ManoranjanJena24 <mjena@mitaoe.ac.in>
2024-02-05 16:38:12 +0530

    Done with CRUD application using REST API Integration

--- RestApi/app.js
+++ RestApi/app.js
@@ -21,8 +21,75 @@ const productSchema = new mongoose.Schema({
 
 const Product = new mongoose.model("Product", productSchema)
 
+
+//  Create Product
 app.post("/api/v1/product/new", async (req, res) => {
-    await Product.create(req.body)
+    const product = await Product.create(req.body)
+    res.status(201).json({
+        success: true,
+        product
+
+    })
+
+})
+
+//Read Product
+app.get("/api/v1/products", async (req, res) => {
+
+    const products = await Product.find();
+
+    res.status(200).json({
+        success: true,
+        products
+    })
+
+})
+
+//Update Product
+app.put("/api/v1/product/:id", async (req, res) => {
+    let product = await Product.findById(req.params.id)
+    if (!product) {
+        return res.status(500).json({
+            success: false,
+            message: "Product Not Found"
+
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        useFindAndModify: false,
+        runValidators: true
+
+    })
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+
+})
+
+
+//Delete Product 
+app.delete("/api/v1/product/:id", async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (!product) {
+        return res.status(500).json({
+            success: false,
+            message: "Product Not Found"
+
+        })
+    }
+
+    await product.deleteOne();
+
+
+    res.status(200).json({
+        success: true,
+        message: "Product is deleted successfully"
+    })
+
 })
 
 
